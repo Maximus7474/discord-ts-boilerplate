970fa574159a36961a1530deddb57aa56861c176
Maximus7474 <the.maximus.7474@gmail.com>
2025-04-24 22:21:40 +0200

    fix(events/ready): set to fire "on" and not "once"

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,7 +4,7 @@ import EventHandler from "../classes/event_handler";
 export default new EventHandler (
     'READY',
     Events.ClientReady,
-    "on",
+    "once",
     (logger, client) => {
         logger.success(`Booted up and logged in as @${client.user?.username}#${client.user?.discriminator}`)
     }
