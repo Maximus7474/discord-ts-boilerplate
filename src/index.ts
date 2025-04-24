39c73576d4b6a78596d0901503dff05605c6fb64
Maximus7474 <the.maximus.7474@gmail.com>
2025-04-24 14:54:30 +0200

    feat(index): added login process for bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+import { DiscordClient } from './types/client';
 import Config from './utils/config';
+import { Client, Collection, Events, GatewayIntentBits, Partials } from 'discord.js';
+
+const client: DiscordClient = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers,
+    ],
+    partials: [
+        Partials.Message,
+        Partials.Channel,
+        Partials.Reaction,
+    ],
+});
+
+client.commands = new Collection();
+
+client.login(Config.DISCORD_BOT_TOKEN);
+
+client.once(Events.ClientReady, (client) => {
+    console.log(`Successfully booted up and logged in as @${client.user.username}#${client.user.discriminator}`)
+});
