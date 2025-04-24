88ef7e7a3e6d5311a1fa4ad1b318b80d1b5ba488
Maximus7474 <the.maximus.7474@gmail.com>
2025-04-24 21:33:41 +0200

    chore(utils/init/load_commands): added logger module to it

--- src/utils/initialisation/load_commands.ts
+++ src/utils/initialisation/load_commands.ts
@@ -3,6 +3,9 @@ import SlashCommand from '../../classes/slash_command';
 import { DiscordClient } from '../../types/client';
 import { getFilesFromDir } from '../utils';
 
+import Logger from '../logger';
+const logger = new Logger('LoadCommands');
+
 export default (client: DiscordClient) => {
     const commands = getFilesFromDir(path.join(__dirname, '../../commands'));
 
@@ -15,6 +18,8 @@ export default (client: DiscordClient) => {
                 const { default: command } = commandModule as { default: SlashCommand };
 
                 client.commands.set(command.register().name, command);
+
+                logger.success(`Loaded /${command.register().name}`);
             }
         } catch (error) {
             console.error(`Failed to load command ${file}:`, error);
