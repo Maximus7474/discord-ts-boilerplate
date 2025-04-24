d610e4b276b3a70294f75d3d2a0193509b733aa8
Maximus7474 <the.maximus.7474@gmail.com>
2025-04-24 17:41:52 +0200

    chore(index): removed unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import { Client, Collection, Events, GatewayIntentBits, Partials } from 'discord.js';
+import { Client, Collection, GatewayIntentBits, Partials } from 'discord.js';
 
 import { DiscordClient } from './types/client';
 import Config from './utils/config';
 
 import LoadCommands from './utils/initialisation/load_commands';
 import LoadEvents from './utils/initialisation/load_events';
-
-import Logger from './utils/logger';
-const logger = new Logger('INDEX');
 
 const client: DiscordClient = new Client({
     intents: [
