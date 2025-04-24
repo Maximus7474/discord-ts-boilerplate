842285595ad195f621de5fb556ab71c3a66528e5
Maximus7474 <the.maximus.7474@gmail.com>
2025-04-24 16:38:33 +0200

    refactor(types/client): DiscordClient commands collection typing

--- src/types/client.ts
+++ src/types/client.ts
@@ -1,7 +1,8 @@
 import { Client, Collection } from "discord.js";
+import SlashCommand from "../classes/slash_command";
 
-export type CommandCollection = Collection<string, { execute: (...args: any[]) => void }>;
+export type CommandCollection = Collection<string, SlashCommand>;
 
 export interface DiscordClient extends Client {
-    commands?: CommandCollection;
+    commands: CommandCollection;
 }
