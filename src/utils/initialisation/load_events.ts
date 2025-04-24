c21d81771323b6bf60e24ba1c9e22209c22f08bc
Maximus7474 <the.maximus.7474@gmail.com>
2025-04-24 17:37:15 +0200

    fix(utils/init/events): added logger module and removed references to commands

--- src/utils/initialisation/load_events.ts
+++ src/utils/initialisation/load_events.ts
@@ -3,24 +3,32 @@ import { DiscordClient } from '../../types/client';
 import { getFilesFromDir } from '../utils';
 import EventHandler from '../../classes/event_handler';
 
-export default (client: DiscordClient) => {
-    const commands = getFilesFromDir(path.join(__dirname, '../../events'));
+import Logger from '../logger';
+const logger = new Logger('LoadEvents');
 
-    commands.forEach((file) => {
+export default (client: DiscordClient) => {
+    const eventDir = path.join(__dirname, '../../events');
+    const events = getFilesFromDir(eventDir);
+
+    events.forEach((file) => {
         const filePath = path.join(file);
         try {
             const eventModule = require(filePath);
 
-            if (eventModule && eventModule.default) {
+            if (!eventModule && eventModule.default) {
                 const { default: event } = eventModule as { default: EventHandler };
 
                 const eventData = event.register();
                 if (eventData.type === 'once') {
                     client.once(eventData.event, (...args) => event.call(client, ...args))
-                } 
+                }
+
+                logger.success(`Loaded ${eventData.name} for event: ${eventData.event}`)
+            } else {
+                logger.warn(`Unable to load event: ${filePath.slice(eventDir.length + 1)}`)
             }
         } catch (error) {
-            console.error(`Failed to load command ${file}:`, error);
+            console.error(`Failed to load event: ${file}\n`, error);
         }
     })
 }
