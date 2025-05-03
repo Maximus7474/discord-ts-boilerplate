87610133dbae746574bf3fae546f2963b6b2ed7d
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-03 19:16:15 +0200

    doc(classes/event_handler): added details on run, get, all

--- src/classes/event_handler.ts
+++ src/classes/event_handler.ts
@@ -3,6 +3,10 @@ import { DiscordClient } from "../types/client";
 
 import Logger from "../utils/logger";
 
+/**
+ * Represents an event handler for a Discord client.
+ * This class is used to define and manage events, including their setup and execution.
+ */
 export default class EventHandler {
 
     private logger: Logger;
@@ -12,6 +16,15 @@ export default class EventHandler {
     private setup?: (logger: Logger, client: DiscordClient) => void;
     private callback: (logger: Logger, client: DiscordClient, ...args: any[]) => void;
 
+    /**
+     * Constructs an instance of the event handler.
+     *
+     * @param name - The name of the event handler.
+     * @param eventName - The name of the event from the `ClientEvents` that this handler listens to.
+     * @param type - Specifies whether the event handler should use "on" or "once" for the event listener.
+     * @param callback - The function to be executed when the event is triggered. Receives a logger, the Discord client, and additional event arguments.
+     * @param setup - (Optional) A function to perform setup logic. Receives a logger and the Discord client.
+     */
     constructor (
         name: string,
         eventName: keyof ClientEvents,
