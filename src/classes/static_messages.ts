c0e36bf45f64a584427e46617357db0b1ee5775b
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-04 11:43:31 +0200

    feat(classes): static message class -> Allow generating static messages and handle interactions from these.

---
+++ src/classes/static_messages.ts
@@ -0,0 +1,42 @@
+import { DiscordClient } from "../types/client";
+
+import Logger from "../utils/logger";
+import { magenta } from "../utils/console_colours";
+import { AnySelectMenuInteraction, ButtonInteraction } from "discord.js";
+
+export default class StaticMessage {
+
+    private logger: Logger;
+    public customIds: string[];
+    public name: string;
+    private setup: (logger: Logger, client: DiscordClient) => Promise<void>;
+    private callback: (logger: Logger, client: DiscordClient, interaction: ButtonInteraction|AnySelectMenuInteraction) => Promise<void>
+
+    constructor (
+        name: string,
+        customIds: string[],
+        setup: (logger: Logger, client: DiscordClient) => Promise<void>,
+        callback: (logger: Logger, client: DiscordClient, interaction: ButtonInteraction|AnySelectMenuInteraction) => Promise<void>,
+    ) {
+        this.name = name;
+        this.logger = new Logger(`${magenta('MSG')}:${name}`);
+        this.customIds = customIds || [];
+        if (!setup) {
+            throw new Error("StaticMessage setup function is required.");
+        }
+        this.setup = setup;
+        this.callback = callback;
+    }
+
+    async initialize (client: DiscordClient): Promise<void> {
+        await this.setup(this.logger, client);
+    }
+
+    getCustomIds (): string[] {
+        return this.customIds;
+    }
+
+    handleInteraction (client: DiscordClient, interaction: ButtonInteraction|AnySelectMenuInteraction): Promise<void> {
+        return this.callback(this.logger, client, interaction);
+    }
+}
