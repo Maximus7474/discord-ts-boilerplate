8552416688ea30bfe0b5852b9d2884fccc4b560f
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-04 12:32:34 +0200

    refactor(database): add block scope to SQLITE case in initDB function

--- src/utils/database/index.ts
+++ src/utils/database/index.ts
@@ -8,10 +8,11 @@ const initDB = (): SQLiteHandler|null => {
     if (!Config.DATABASE_PROTOCOL) return null;
 
     switch (Config.DATABASE_PROTOCOL) {
-        case 'SQLITE':
+        case 'SQLITE': {
             logger.info('Loading SQLite Handler');
             const Database = new SQLiteHandler(Config.SQLITE_PATH);
             return Database as SQLiteHandler;
+        }
         default:
             throw new Error(`Invalid DATABASE_PROTOCOL: ${Config.DATABASE_PROTOCOL}`);
     }
