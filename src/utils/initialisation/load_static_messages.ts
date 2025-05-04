eef9f01031fa86722e694c7c3b6b010bffae94c8
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-04 11:55:22 +0200

    feat(static_messages): implement static message loading and interaction handling

---
+++ src/utils/initialisation/load_static_messages.ts
@@ -0,0 +1,58 @@
+import * as path from 'path';
+import { DiscordClient } from '../../types/client';
+import { getFilesFromDir } from '../utils';
+
+import Logger from '../logger';
+import StaticMessage from '../../classes/static_messages';
+import { AnySelectMenuInteraction, ButtonInteraction } from 'discord.js';
+const logger = new Logger('LoadStaticMessages');
+
+export default (client: DiscordClient) => {
+    const initialiseStaticMessage = async (client: DiscordClient) => {
+        const callbackHandler = new Map<string, (client: DiscordClient, interaction: ButtonInteraction|AnySelectMenuInteraction) => Promise<void>>();
+    
+        const staticMessageDir = path.join(__dirname, '../../static_messages');
+        const messages = getFilesFromDir(staticMessageDir);
+    
+        messages.forEach(async (file) => {
+            const filePath = path.join(file);
+            try {
+                const staticMessageModule = require(filePath);
+    
+                if (staticMessageModule && staticMessageModule.default) {
+                    const { default: message } = staticMessageModule as { default: StaticMessage };
+    
+                    await message.initialize(client);
+    
+                    message.customIds.forEach((customId) => {
+                        callbackHandler.set(customId, message.handleInteraction);
+                    });
+
+                    logger.success(`Loaded static message: ${message.name}`)
+                } else {
+                    logger.warn(`Unable to static message: ${filePath.slice(staticMessageDir.length + 1)}`)
+                }
+            } catch (error) {
+                console.error(`Failed to load static message: ${file}\n`, error);
+            }
+        });
+    
+        client.on('interactionCreate', async (interaction) => {
+            if (interaction.isButton() || interaction.isAnySelectMenu()) {
+                const { customId } = interaction;
+                const handler = callbackHandler.get(customId);
+                console.log(customId, callbackHandler.keys());
+                if (handler) {
+                    await handler(client, interaction as ButtonInteraction|AnySelectMenuInteraction);
+                } else {
+                    logger.warn(`No handler found for static message: ${customId}`);
+                }
+            }
+        });
+    }
+
+    client.once('ready', async () => {
+        await initialiseStaticMessage(client);
+    });
+}
+
