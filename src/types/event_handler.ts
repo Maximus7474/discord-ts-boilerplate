e8fc086d885155d75d9973a6c18e7f08078383b6
Maximus7474 <the.maximus.7474@gmail.com>
2025-05-04 12:32:02 +0200

    feat(event_handler): add types for event handler setup and callback

---
+++ src/types/event_handler.ts
@@ -0,0 +1,7 @@
+import Logger from "../utils/logger";
+import { DiscordClient } from "./client";
+
+export type EventHandlerSetup = (logger: Logger, client: DiscordClient) => void;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type EventHandlerCallback = (logger: Logger, client: DiscordClient, ...args: any[]) => void;
