ee35291344fe24e0f72d03abcebf6176ba530efe
Maximus7474 <the.maximus.7474@gmail.com>
2025-06-03 12:32:23 +0200

    fix(init/load_commands): ESM path error

--- src/utils/initialisation/load_commands.ts
+++ src/utils/initialisation/load_commands.ts
@@ -4,6 +4,7 @@ import { DiscordClient } from '../../types';
 import { getFilesFromDir } from '../utils';
 
 import Logger from '../logger';
+import { pathToFileURL } from 'url';
 const logger = new Logger('LoadCommands');
 
 export default (client: DiscordClient) => {
@@ -11,8 +12,10 @@ export default (client: DiscordClient) => {
 
     commands.forEach(async (file) => {
         const filePath = path.join(file);
+        const fileUrl = pathToFileURL(filePath).href;
+
         try {
-            const commandModule = await import(filePath);
+            const commandModule = await import(fileUrl);
 
             if (commandModule && commandModule.default) {
                 const { default: command } = commandModule as { default: SlashCommand };
