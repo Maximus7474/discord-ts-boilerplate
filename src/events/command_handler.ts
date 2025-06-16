7951f21942b092346715b59094c0d999547f51a3
Maximus7474 <the.maximus.7474@gmail.com>
2025-06-16 22:17:31 +0200

    feat(events): added handling of Autocomplete Interactions

--- src/events/command_handler.ts
+++ src/events/command_handler.ts
@@ -1,16 +1,25 @@
-import { Events } from "discord.js";
+import { AutocompleteInteraction, ChatInputCommandInteraction, Events } from "discord.js";
 import EventHandler from "../classes/event_handler";
 
 export default new EventHandler (
     'COMMANDS',
     Events.InteractionCreate,
     "on",
-    (logger, client, interaction) => {
-        if (!interaction.isChatInputCommand()) return;
-        
-        const command = client.commands.get(interaction.commandName);
-        if (!command) return;
+    (logger, client, interaction: ChatInputCommandInteraction | AutocompleteInteraction) => {
+        if (interaction.isChatInputCommand()) {
+            const commandCallback = client.commands.get(interaction.commandName);
+            if (!commandCallback) return;
 
-        command(client, interaction);
+            commandCallback(client, interaction);
+            return;
+        }
+        
+        if (interaction.isAutocomplete()) {
+            const commandAutoComplete = client.autocompleteCommands.get(interaction.commandName);
+            if (!commandAutoComplete) return;
+
+            commandAutoComplete(client, interaction);
+            return;
+        }
     }
 );
