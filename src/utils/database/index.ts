2fe730094098cca67679ec651079a55844b5b1ec
Maximus7474 <the.maximus.7474@gmail.com>
2025-06-24 12:16:07 +0200

    fix(database): passing all SQL config fields to the DBHandler class constructor

--- src/utils/database/index.ts
+++ src/utils/database/index.ts
@@ -1,8 +1,16 @@
+import { DBConnectionDetails } from '@types';
 import Config from '../config';
 import DBHandler from './handler';
 
-const Database = new DBHandler({
+const connectionDetails: DBConnectionDetails = {
     SQLITE_PATH: Config.SQLITE_PATH,
-});
+    SQL_HOST: Config.SQL_HOST,
+    SQL_PORT: Config.SQL_PORT,
+    SQL_USER: Config.SQL_USER,
+    SQL_DATABASE: Config.SQL_DATABASE,
+    SQL_PASSWORD: Config.SQL_PASSWORD,
+};
+
+const Database = new DBHandler(connectionDetails);
 
 export default Database;
