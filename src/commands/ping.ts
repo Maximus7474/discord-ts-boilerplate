f9737f09a5a30c0e8b9882a9a2c7f9e1504af31a
Maximus7474 <the.maximus.7474@gmail.com>
2025-08-18 10:19:18 +0200

    feat(commands/ping): command localization

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,21 @@
 import { MessageFlags, SlashCommandBuilder } from "discord.js";
 import SlashCommand from "../classes/slash_command";
+import { CommandCallbackLocalizations, CommandLocalization } from "@types";
+import { LocalizeString } from "../utils/utils";
+
+const command_localization: CommandLocalization = {
+    name: {},
+    description: {
+        'fr': 'Envoyez une requête ping au bot pour vérifier s\'il est actif !',
+        'de': 'Pingen Sie den Bot an, um zu überprüfen, ob er aktiv ist!',
+    }
+};
+
+const callback_localizations: CommandCallbackLocalizations = {
+    response: {
+        default: 'Pong ! ({delay} ms)',
+    },
+}
 
 export default new SlashCommand({
     name: 'ping',
@@ -7,13 +23,18 @@ export default new SlashCommand({
     hideFromHelp: false,
     slashcommand: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Ping the bot to check if it is alive !'),
+        .setNameLocalizations(command_localization.name)
+        .setDescription('Ping the bot to check if it is alive !')
+        .setDescriptionLocalizations(command_localization.description),
     callback: async (logger, client, interaction) => {
         logger.success('Successfully received usage of /ping from discord API');
         await interaction.reply({
             // Ping is calculated by subtracting the current timestamp from the interaction created timestamp
             // This is not the best way to calculate ping, but it is a good approximation
-            content: `Pong ! (${interaction.createdTimestamp - Date.now()} ms)`,
+            content: LocalizeString(
+                callback_localizations.response, interaction.locale,
+                { delay: interaction.createdTimestamp - Date.now() },
+            ),
             flags: MessageFlags.Ephemeral,
         })
     }
